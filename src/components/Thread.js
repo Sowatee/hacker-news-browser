f2a1698625a1608584c30076ea833eaf088fc366
Sowatee <jamesprescott9000@gmail.com>
2022-07-09 17:50:26 -0500

    Added data from API

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -2,20 +2,22 @@ import styled from "styled-components";
 import { MdArrowCircleUp, MdOutlineModeComment} from "react-icons/md";
 
 
-export default function Thread() {
+export default function Thread({title, author, num_comments, points, url, objectID}) {
 
   return (
     <Wrapper>
-      <h4>Lorem ipsum dolor sit amet.</h4>
+      <h4>{title}</h4>
       <footer>
         <div>
           <MdArrowCircleUp />
-          <span>345</span>
+          <span>{points}</span>
         </div>
-        <button>hide</button>
         <div>
           <MdOutlineModeComment />
-          <span>300</span>
+          <a className="comments" href={`https://news.ycombinator.com/item?id=${objectID}`} target="_blank">{num_comments}</a>
+        </div>
+        <div>
+          <a href={url || `https://news.ycombinator.com/item?id=${objectID}`} target="_blank">read more</a>
         </div> 
       </footer>
     </Wrapper>
@@ -26,11 +28,12 @@ const Wrapper = styled.article`
   @media (min-width: 500px) {
     max-width: 500px;
   }
-  width: 90vw;
+  width: 80vw;
+  height: 15vh;
   margin: auto;
-  padding: .3rem 1rem;
+  padding: .5rem 1rem;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   flex-direction: column;  
   border: 1px solid black;
   border-radius: .5rem;
@@ -38,30 +41,30 @@ const Wrapper = styled.article`
   h4 {
     font-size: 1.2rem;
     margin-top: 0;
-
-    &:hover {
-      cursor: pointer;
-      text-decoration: underline;
-    }
   }
+
   footer {
     display: flex;
+    justify-content: space-evenly;
     gap: 3rem;
 
     div {
       display: flex;
       gap: .5rem;
     }
-  }
 
-  button {
-    border: none;
-    background: none;
+    .comments {
+      color: black;
+    }
 
-    &:hover {
-      cursor: pointer;
-      text-decoration: underline;
-      color: red;
+    a {
+      color: blue;
+      text-transform: capitalize;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
     }
   }
 `
